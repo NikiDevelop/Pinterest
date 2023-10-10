af20ea2de65990f4869edb7ae0a9e543ecf117f6
Nikifor Genchev <105102619+NikiDevelop@users.noreply.github.com>
2023-10-10 09:37:30 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pinterest-clone.azurewebsites.net', '*']
 
 
 
